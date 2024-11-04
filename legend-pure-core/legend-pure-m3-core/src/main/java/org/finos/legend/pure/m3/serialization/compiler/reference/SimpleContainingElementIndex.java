c47bef7fb490d191812f8b10bf1bf497d6ede5a8
kevin-m-knight-gs <kevin.knight@gs.com>
2024-11-04 18:18:48 -0500

    Add containing element indexes

---
+++ legend-pure-core/legend-pure-m3-core/src/main/java/org/finos/legend/pure/m3/serialization/compiler/reference/SimpleContainingElementIndex.java
@@ -0,0 +1,243 @@
+// Copyright 2024 Goldman Sachs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.finos.legend.pure.m3.serialization.compiler.reference;
+
+import org.eclipse.collections.api.factory.Lists;
+import org.eclipse.collections.api.factory.Maps;
+import org.eclipse.collections.api.factory.Sets;
+import org.eclipse.collections.api.list.ImmutableList;
+import org.eclipse.collections.api.list.MutableList;
+import org.eclipse.collections.api.map.MapIterable;
+import org.eclipse.collections.api.map.MutableMap;
+import org.eclipse.collections.api.set.SetIterable;
+import org.finos.legend.pure.m3.navigation.PackageableElement.PackageableElement;
+import org.finos.legend.pure.m3.navigation.ProcessorSupport;
+import org.finos.legend.pure.m3.navigation._package._Package;
+import org.finos.legend.pure.m4.coreinstance.CoreInstance;
+import org.finos.legend.pure.m4.coreinstance.SourceInformation;
+
+import java.util.Objects;
+
+class SimpleContainingElementIndex implements ContainingElementIndex
+{
+    private final MapIterable<String, ImmutableList<CoreInstance>> sourceIndex;
+    private final SetIterable<CoreInstance> virtualPackages;
+
+    private SimpleContainingElementIndex(MapIterable<String, ImmutableList<CoreInstance>> sourceIndex, SetIterable<CoreInstance> virtualPackages)
+    {
+        this.sourceIndex = sourceIndex;
+        this.virtualPackages = virtualPackages;
+    }
+
+    @Override
+    public boolean equals(Object other)
+    {
+        if (this == other)
+        {
+            return true;
+        }
+        if (!(other instanceof SimpleContainingElementIndex))
+        {
+            return false;
+        }
+        SimpleContainingElementIndex that = (SimpleContainingElementIndex) other;
+        return this.sourceIndex.equals(that.sourceIndex) && this.virtualPackages.equals(that.virtualPackages);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return this.sourceIndex.hashCode() + (31 * this.virtualPackages.hashCode());
+    }
+
+    @Override
+    public String toString()
+    {
+        StringBuilder builder = new StringBuilder(getClass().getSimpleName()).append('{');
+        this.sourceIndex.forEachKeyValue((source, elements) ->
+        {
+            builder.append(source).append(":[");
+            elements.forEach(element ->
+            {
+                PackageableElement.writeUserPathForPackageableElement(builder, element);
+                element.getSourceInformation().appendIntervalMessage(builder.append(" (")).append("), ");
+            });
+            builder.setLength(builder.length() - 2);
+            builder.append("], ");
+        });
+        if (this.virtualPackages.isEmpty())
+        {
+            builder.setLength(builder.length() - 2);
+            builder.append('}');
+        }
+        else
+        {
+            builder.append("virtual packages:[");
+            this.virtualPackages.forEach(pkg -> PackageableElement.writeUserPathForPackageableElement(builder, pkg).append(", "));
+            builder.setLength(builder.length() - 2);
+            builder.append("]}");
+        }
+        return builder.toString();
+    }
+
+    @Override
+    public CoreInstance findContainingElement(CoreInstance instance)
+    {
+        SourceInformation sourceInfo = instance.getSourceInformation();
+        if (sourceInfo != null)
+        {
+            ImmutableList<CoreInstance> sourceElements = this.sourceIndex.get(sourceInfo.getSourceId());
+            if (sourceElements != null)
+            {
+                // Binary search among elements sorted by position in the source
+                int low = 0;
+                int high = sourceElements.size() - 1;
+                while (low <= high)
+                {
+                    int mid = (low + high) >>> 1;
+                    CoreInstance element = sourceElements.get(mid);
+                    if (element == instance)
+                    {
+                        return element;
+                    }
+
+                    SourceInformation elementSourceInfo = element.getSourceInformation();
+                    if (SourceInformation.isBefore(sourceInfo.getEndLine(), sourceInfo.getEndColumn(), elementSourceInfo.getStartLine(), elementSourceInfo.getStartColumn()))
+                    {
+                        // instance ends before element starts
+                        high = mid - 1;
+                    }
+                    else if (SourceInformation.isAfter(sourceInfo.getStartLine(), sourceInfo.getStartColumn(), elementSourceInfo.getEndLine(), elementSourceInfo.getEndColumn()))
+                    {
+                        // instance starts after element ends
+                        low = mid + 1;
+                    }
+                    else
+                    {
+                        // instance and element must intersect, which by assumption means that element contains instance
+                        return element;
+                    }
+                }
+            }
+        }
+        else if (this.virtualPackages.contains(instance))
+        {
+            return instance;
+        }
+        return null;
+    }
+
+    static Builder builder(ProcessorSupport processorSupport)
+    {
+        return new Builder(processorSupport);
+    }
+
+    static class Builder
+    {
+        private final MutableMap<String, MutableList<CoreInstance>> elementsBySource = Maps.mutable.empty();
+        private final MutableList<CoreInstance> virtualPackages = Lists.mutable.empty();
+        private final ProcessorSupport processorSupport;
+
+        private Builder(ProcessorSupport processorSupport)
+        {
+            this.processorSupport = processorSupport;
+        }
+
+        void addElement(CoreInstance element)
+        {
+            SourceInformation sourceInfo = Objects.requireNonNull(element, "element may not be null").getSourceInformation();
+            if (sourceInfo == null)
+            {
+                if (_Package.isPackage(element, this.processorSupport))
+                {
+                    this.virtualPackages.add(element);
+                    return;
+                }
+                throw new IllegalArgumentException("Invalid element, no source information: " + element);
+            }
+            if (!sourceInfo.isValid())
+            {
+                throw new IllegalArgumentException("Element with invalid source information: " + element + ", " + sourceInfo.getMessage());
+            }
+            this.elementsBySource.getIfAbsentPut(sourceInfo.getSourceId(), Lists.mutable::empty).add(element);
+        }
+
+        Builder withElement(CoreInstance element)
+        {
+            addElement(element);
+            return this;
+        }
+
+        void addElements(Iterable<? extends CoreInstance> elements)
+        {
+            elements.forEach(this::addElement);
+        }
+
+        Builder withElements(Iterable<? extends CoreInstance> elements)
+        {
+            addElements(elements);
+            return this;
+        }
+
+        SimpleContainingElementIndex build()
+        {
+            MutableMap<String, ImmutableList<CoreInstance>> finalIndex = Maps.mutable.ofInitialCapacity(this.elementsBySource.size());
+            this.elementsBySource.forEachKeyValue((source, elements) ->
+            {
+                if (elements.size() > 1)
+                {
+                    elements.sortThis(Builder::compareByStartPosition);
+                    CoreInstance[] prev = new CoreInstance[1];
+                    elements.removeIf(current ->
+                    {
+                        CoreInstance previous = prev[0];
+                        if (current == previous)
+                        {
+                            return true;
+                        }
+                        if ((previous != null) && overlaps(current, previous))
+                        {
+                            StringBuilder builder = new StringBuilder("Distinct elements with overlapping source information: ");
+                            PackageableElement.writeUserPathForPackageableElement(builder, previous);
+                            previous.getSourceInformation().appendMessage(builder.append(" (")).append("), ");
+                            PackageableElement.writeUserPathForPackageableElement(builder, current);
+                            current.getSourceInformation().appendMessage(builder.append(" (")).append(")");
+                            throw new IllegalStateException(builder.toString());
+                        }
+                        prev[0] = current;
+                        return false;
+                    });
+                }
+                finalIndex.put(source, elements.toImmutable());
+            });
+            return new SimpleContainingElementIndex(finalIndex, Sets.mutable.withAll(this.virtualPackages));
+        }
+
+        private static int compareByStartPosition(CoreInstance element1, CoreInstance element2)
+        {
+            SourceInformation sourceInfo1 = element1.getSourceInformation();
+            SourceInformation sourceInfo2 = element2.getSourceInformation();
+            return SourceInformation.comparePositions(sourceInfo1.getStartLine(), sourceInfo1.getStartColumn(), sourceInfo2.getStartLine(), sourceInfo2.getStartColumn());
+        }
+
+        private static boolean overlaps(CoreInstance element1, CoreInstance element2)
+        {
+            SourceInformation sourceInfo1 = element1.getSourceInformation();
+            SourceInformation sourceInfo2 = element2.getSourceInformation();
+            return SourceInformation.isNotAfter(sourceInfo1.getStartLine(), sourceInfo1.getStartColumn(), sourceInfo2.getEndLine(), sourceInfo2.getEndColumn()) &&
+                    SourceInformation.isNotBefore(sourceInfo1.getEndLine(), sourceInfo1.getEndColumn(), sourceInfo2.getStartLine(), sourceInfo2.getStartColumn());
+        }
+    }
+}
