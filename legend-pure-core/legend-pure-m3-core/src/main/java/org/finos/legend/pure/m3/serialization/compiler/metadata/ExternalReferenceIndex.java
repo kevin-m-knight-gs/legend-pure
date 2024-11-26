4ad8528134bc4074bc675a6f2156b748feef8120
kevin-m-knight-gs <kevin.knight@gs.com>
2024-11-26 16:54:07 -0500

    Add metadata index

--- legend-pure-core/legend-pure-m3-core/src/main/java/org/finos/legend/pure/m3/serialization/compiler/metadata/ExternalReferenceIndex.java
+++ legend-pure-core/legend-pure-m3-core/src/main/java/org/finos/legend/pure/m3/serialization/compiler/metadata/ExternalReferenceIndex.java
@@ -21,16 +21,14 @@ import org.eclipse.collections.api.list.ImmutableList;
 import org.eclipse.collections.api.list.MutableList;
 import org.eclipse.collections.api.map.MapIterable;
 import org.eclipse.collections.api.map.MutableMap;
-import org.finos.legend.pure.m3.navigation.graph.GraphPath;
 
 import java.util.function.BiConsumer;
-import java.util.function.Consumer;
 
 class ExternalReferenceIndex
 {
-    private final MapIterable<String, ImmutableList<ExternalReference>> index;
+    private final MapIterable<String, ImmutableList<String>> index;
 
-    private ExternalReferenceIndex(MapIterable<String, ImmutableList<ExternalReference>> index)
+    private ExternalReferenceIndex(MapIterable<String, ImmutableList<String>> index)
     {
         this.index = index;
     }
@@ -45,128 +43,29 @@ class ExternalReferenceIndex
         return this.index.keysView();
     }
 
-    RichIterable<ExternalReference> getAllExternalReferences()
+    ImmutableList<String> getAllReferencesTo(String referenceId)
     {
-        return this.index.valuesView().flatCollect(exRefs -> exRefs);
-    }
-
-    ImmutableList<ExternalReference> getAllReferencesTo(String referenceId)
-    {
-        ImmutableList<ExternalReference> refs = this.index.get(referenceId);
+        ImmutableList<String> refs = this.index.get(referenceId);
         return (refs == null) ? Lists.immutable.empty() : refs;
     }
 
-    void forEachExternalReference(Consumer<? super ExternalReference> consumer)
+    void forEachExternalReference(BiConsumer<? super String, ? super String> consumer)
     {
-        this.index.forEachValue(refs -> refs.forEach(consumer));
+        this.index.forEachKeyValue((ref, owners) -> owners.forEach(owner -> consumer.accept(ref, owner)));
     }
 
-    void forEachIdWithExternalReferences(BiConsumer<? super String, ? super ImmutableList<ExternalReference>> consumer)
+    void forEachIdWithReferencesTo(BiConsumer<? super String, ? super ImmutableList<String>> consumer)
     {
         this.index.forEachKeyValue(consumer::accept);
     }
 
     static ExternalReferenceIndex buildIndex(ElementIndex elementIndex)
     {
-        MutableMap<String, MutableList<ExternalReference>> initialIndex = Maps.mutable.empty();
-        elementIndex.forEachElement(e -> e.getExternalReferences().forEach(extRef -> initialIndex.getIfAbsentPut(extRef.getReferenceId(), Lists.mutable::empty).add(extRef)));
+        MutableMap<String, MutableList<String>> initialIndex = Maps.mutable.empty();
+        elementIndex.forEachElement(e -> e.getExternalReferences().forEach(refId -> initialIndex.getIfAbsentPut(refId, Lists.mutable::empty).add(e.getPath())));
 
-        MutableMap<String, ImmutableList<ExternalReference>> index = Maps.mutable.ofInitialCapacity(initialIndex.size());
-        initialIndex.forEachKeyValue((refId, graphPaths) -> index.put(refId, graphPaths.sortThis((r1, r2) -> compareGraphPaths(r1.getPath(), r2.getPath())).toImmutable()));
+        MutableMap<String, ImmutableList<String>> index = Maps.mutable.ofInitialCapacity(initialIndex.size());
+        initialIndex.forEachKeyValue((refId, refOwners) -> index.put(refId, refOwners.sortThis().toImmutable()));
         return new ExternalReferenceIndex(index);
-    }
-
-    private static int compareGraphPaths(GraphPath path1, GraphPath path2)
-    {
-        int cmp = path1.getStartNodePath().compareTo(path2.getStartNodePath());
-        if (cmp != 0)
-        {
-            return cmp;
-        }
-
-        cmp = Integer.compare(path1.getEdgeCount(), path2.getEdgeCount());
-        if (cmp != 0)
-        {
-            return cmp;
-        }
-
-        for (int i = 0, len = path1.getEdgeCount(); i < len; i++)
-        {
-            cmp = compareEdges(path1.getEdge(i), path2.getEdge(i));
-            if (cmp != 0)
-            {
-                return cmp;
-            }
-        }
-        return 0;
-    }
-
-    private static int compareEdges(GraphPath.Edge edge1, GraphPath.Edge edge2)
-    {
-        int cmp = edge1.getProperty().compareTo(edge2.getProperty());
-        if (cmp != 0)
-        {
-            return cmp;
-        }
-
-        return edge1.visit(new GraphPath.EdgeVisitor<Integer>()
-        {
-            @Override
-            public Integer visit(GraphPath.ToOnePropertyEdge e1)
-            {
-                return (edge2 instanceof GraphPath.ToOnePropertyEdge) ? 0 : -1;
-            }
-
-            @Override
-            public Integer visit(GraphPath.ToManyPropertyAtIndexEdge e1)
-            {
-                return edge2.visit(new GraphPath.EdgeVisitor<Integer>()
-                {
-                    @Override
-                    public Integer visit(GraphPath.ToOnePropertyEdge e2)
-                    {
-                        return 1;
-                    }
-
-                    @Override
-                    public Integer visit(GraphPath.ToManyPropertyAtIndexEdge e2)
-                    {
-                        return Integer.compare(e1.getIndex(), e2.getIndex());
-                    }
-
-                    @Override
-                    public Integer visit(GraphPath.ToManyPropertyWithStringKeyEdge e2)
-                    {
-                        return -1;
-                    }
-                });
-            }
-
-            @Override
-            public Integer visit(GraphPath.ToManyPropertyWithStringKeyEdge e1)
-            {
-                return edge2.visit(new GraphPath.EdgeVisitor<Integer>()
-                {
-                    @Override
-                    public Integer visit(GraphPath.ToOnePropertyEdge e2)
-                    {
-                        return 1;
-                    }
-
-                    @Override
-                    public Integer visit(GraphPath.ToManyPropertyAtIndexEdge e2)
-                    {
-                        return 1;
-                    }
-
-                    @Override
-                    public Integer visit(GraphPath.ToManyPropertyWithStringKeyEdge e2)
-                    {
-                        int cmp = e1.getKeyProperty().compareTo(e2.getKeyProperty());
-                        return (cmp != 0) ? cmp : e1.getKey().compareTo(e2.getKey());
-                    }
-                });
-            }
-        });
     }
 }
