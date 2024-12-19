fad4af9a6e11624aa3f4e9cbcb856d40eb1555fa
kevin-m-knight-gs <kevin.knight@gs.com>
2024-12-19 11:46:42 -0500

    Add concrete element serialization

---
+++ legend-pure-core/legend-pure-m3-core/src/main/java/org/finos/legend/pure/m3/serialization/compiler/element/ConcreteElementSerializer.java
@@ -0,0 +1,172 @@
+// Copyright 2024 Goldman Sachs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.finos.legend.pure.m3.serialization.compiler.element;
+
+import org.finos.legend.pure.m3.navigation.ProcessorSupport;
+import org.finos.legend.pure.m3.serialization.compiler.ExtensibleSerializer;
+import org.finos.legend.pure.m3.serialization.compiler.reference.ReferenceIdProvider;
+import org.finos.legend.pure.m3.serialization.compiler.reference.ReferenceIds;
+import org.finos.legend.pure.m3.serialization.compiler.strings.StringIndexer;
+import org.finos.legend.pure.m4.coreinstance.CoreInstance;
+import org.finos.legend.pure.m4.serialization.Reader;
+import org.finos.legend.pure.m4.serialization.Writer;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class ConcreteElementSerializer extends ExtensibleSerializer<ConcreteElementSerializerExtension>
+{
+    private static final long PURE_ELEMENT_SIGNATURE = Long.parseLong("PureElement", 36);
+
+    private final ReferenceIds referenceIds;
+    private final StringIndexer stringIndexer;
+    private final ProcessorSupport processorSupport;
+
+    private ConcreteElementSerializer(Iterable<? extends ConcreteElementSerializerExtension> extensions, int defaultVersion, StringIndexer stringIndexer, ReferenceIds referenceIds, ProcessorSupport processorSupport)
+    {
+        super(extensions, defaultVersion);
+        this.stringIndexer = stringIndexer;
+        this.referenceIds = referenceIds;
+        this.processorSupport = processorSupport;
+    }
+
+    public void serialize(Writer writer, CoreInstance element)
+    {
+        serialize(writer, element, getDefaultExtension(), this.referenceIds.provider());
+    }
+
+    public void serialize(Writer writer, CoreInstance element, int serializerVersion, int referenceIdVersion)
+    {
+        ConcreteElementSerializerExtension serializerExtension = getExtension(serializerVersion);
+        ReferenceIdProvider referenceIdProvider = this.referenceIds.provider(referenceIdVersion);
+        serialize(writer, element, serializerExtension, referenceIdProvider);
+    }
+
+    private void serialize(Writer writer, CoreInstance element, ConcreteElementSerializerExtension serializerExtension, ReferenceIdProvider referenceIdProvider)
+    {
+        writer.writeLong(PURE_ELEMENT_SIGNATURE);
+        writer.writeInt(serializerExtension.version());
+        writer.writeInt(referenceIdProvider.version());
+        serializerExtension.serialize(writer, element, new SerializationContext(this.stringIndexer, referenceIdProvider, this.processorSupport));
+    }
+
+    public DeserializedConcreteElement deserialize(Reader reader)
+    {
+        long signature = reader.readLong();
+        if (signature != PURE_ELEMENT_SIGNATURE)
+        {
+            throw new IllegalArgumentException("Invalid file format: not a Legend concrete element file");
+        }
+        int version = reader.readInt();
+        ConcreteElementSerializerExtension extension = getExtension(version);
+        int referenceIdVersion = reader.readInt();
+        return extension.deserialize(reader, new SerializationContext(this.stringIndexer, this.referenceIds.provider(referenceIdVersion), this.processorSupport));
+    }
+
+    public ReferenceIds getReferenceIds()
+    {
+        return this.referenceIds;
+    }
+
+    public static Builder builder(ProcessorSupport processorSupport)
+    {
+        return new Builder(processorSupport);
+    }
+
+    public static class Builder extends AbstractBuilder<ConcreteElementSerializerExtension, ConcreteElementSerializer>
+    {
+        private StringIndexer stringIndexer;
+        private ReferenceIds referenceIds;
+        private final ProcessorSupport processorSupport;
+
+        private Builder(ProcessorSupport processorSupport)
+        {
+            this.processorSupport = Objects.requireNonNull(processorSupport);
+        }
+
+        public Builder withExtension(ConcreteElementSerializerExtension extension)
+        {
+            addExtension(extension);
+            return this;
+        }
+
+        public Builder withExtensions(Iterable<? extends ConcreteElementSerializerExtension> extensions)
+        {
+            addExtensions(extensions);
+            return this;
+        }
+
+        public Builder withExtensions(ConcreteElementSerializerExtension... extensions)
+        {
+            return withExtensions(Arrays.asList(extensions));
+        }
+
+        public Builder withLoadedExtensions(ClassLoader classLoader)
+        {
+            loadExtensions(classLoader);
+            return this;
+        }
+
+        public Builder withLoadedExtensions()
+        {
+            loadExtensions();
+            return this;
+        }
+
+        public Builder withDefaultVersion(int defaultVersion)
+        {
+            setDefaultVersion(defaultVersion);
+            return this;
+        }
+
+        public Builder withStringIndexer(StringIndexer stringIndexer)
+        {
+            this.stringIndexer = stringIndexer;
+            return this;
+        }
+
+        public Builder withReferenceIds(ReferenceIds referenceIds)
+        {
+            this.referenceIds = referenceIds;
+            return this;
+        }
+
+        @Override
+        protected ConcreteElementSerializer build(Iterable<ConcreteElementSerializerExtension> extensions, int defaultVersion)
+        {
+            return new ConcreteElementSerializer(extensions, defaultVersion, resolveStringIndexer(), resolveReferenceIds(), this.processorSupport);
+        }
+
+        private ReferenceIds resolveReferenceIds()
+        {
+            // If reference ids has not been specified, create one
+            return (this.referenceIds == null) ?
+                   ReferenceIds.builder(this.processorSupport).withAvailableExtensions().build() :
+                   this.referenceIds;
+        }
+
+        private StringIndexer resolveStringIndexer()
+        {
+            // if string indexer has not been specified, use the default
+            return (this.stringIndexer == null) ? StringIndexer.defaultStringIndexer() : this.stringIndexer;
+        }
+
+        @Override
+        protected Class<ConcreteElementSerializerExtension> getExtensionClass()
+        {
+            return ConcreteElementSerializerExtension.class;
+        }
+    }
+}
