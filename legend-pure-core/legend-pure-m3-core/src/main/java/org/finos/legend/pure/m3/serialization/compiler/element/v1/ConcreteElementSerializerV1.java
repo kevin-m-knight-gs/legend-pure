296bdac804b892ee887bd2a7987e028c7b071f04
kevin-m-knight-gs <kevin.knight@gs.com>
2024-12-19 11:46:42 -0500

    Add concrete element serialization

---
+++ legend-pure-core/legend-pure-m3-core/src/main/java/org/finos/legend/pure/m3/serialization/compiler/element/v1/ConcreteElementSerializerV1.java
@@ -0,0 +1,43 @@
+// Copyright 2024 Goldman Sachs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.finos.legend.pure.m3.serialization.compiler.element.v1;
+
+import org.finos.legend.pure.m3.serialization.compiler.element.ConcreteElementSerializerExtension;
+import org.finos.legend.pure.m3.serialization.compiler.element.DeserializedConcreteElement;
+import org.finos.legend.pure.m3.serialization.compiler.element.SerializationContext;
+import org.finos.legend.pure.m4.coreinstance.CoreInstance;
+import org.finos.legend.pure.m4.serialization.Reader;
+import org.finos.legend.pure.m4.serialization.Writer;
+
+public class ConcreteElementSerializerV1 implements ConcreteElementSerializerExtension
+{
+    @Override
+    public int version()
+    {
+        return 1;
+    }
+
+    @Override
+    public void serialize(Writer writer, CoreInstance element, SerializationContext serializationContext)
+    {
+        new SerializerV1(element, serializationContext).serialize(writer);
+    }
+
+    @Override
+    public DeserializedConcreteElement deserialize(Reader reader, SerializationContext serializationContext)
+    {
+        return new DeserializerV1(serializationContext).deserialize(reader);
+    }
+}
