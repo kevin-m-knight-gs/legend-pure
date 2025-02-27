9787a6d233821ebbc87fa16a5e60ff368b7b4840
kevin-m-knight-gs <kevin.knight@gs.com>
2025-02-27 11:14:29 -0500

    Add main serializer

---
+++ legend-pure-core/legend-pure-m3-core/src/main/java/org/finos/legend/pure/m3/serialization/compiler/PureCompilerSerializer.java
@@ -0,0 +1,453 @@
+// Copyright 2025 Goldman Sachs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.finos.legend.pure.m3.serialization.compiler;
+
+import org.eclipse.collections.api.factory.Lists;
+import org.eclipse.collections.api.factory.Maps;
+import org.eclipse.collections.api.factory.Sets;
+import org.eclipse.collections.api.list.ListIterable;
+import org.eclipse.collections.api.list.MutableList;
+import org.eclipse.collections.api.map.MutableMap;
+import org.eclipse.collections.api.set.MutableSet;
+import org.eclipse.collections.api.set.SetIterable;
+import org.eclipse.collections.api.tuple.Pair;
+import org.eclipse.collections.impl.set.mutable.SetAdapter;
+import org.eclipse.collections.impl.tuple.Tuples;
+import org.finos.legend.pure.m3.navigation.PackageableElement.PackageableElement;
+import org.finos.legend.pure.m3.navigation.ProcessorSupport;
+import org.finos.legend.pure.m3.serialization.compiler.file.FileSerializer;
+import org.finos.legend.pure.m3.serialization.compiler.metadata.ModuleMetadata;
+import org.finos.legend.pure.m3.serialization.compiler.metadata.ModuleMetadataGenerator;
+import org.finos.legend.pure.m3.tools.GraphTools;
+import org.finos.legend.pure.m4.coreinstance.CoreInstance;
+
+import java.nio.file.Path;
+import java.util.Objects;
+import java.util.Set;
+import java.util.function.Predicate;
+import java.util.zip.ZipOutputStream;
+
+public class PureCompilerSerializer
+{
+    private final FileSerializer fileSerializer;
+    private final ModuleMetadataGenerator moduleMetadataGenerator;
+    private final ProcessorSupport processorSupport;
+
+    private PureCompilerSerializer(FileSerializer fileSerializer, ModuleMetadataGenerator moduleMetadataGenerator, ProcessorSupport processorSupport)
+    {
+        this.fileSerializer = Objects.requireNonNull(fileSerializer);
+        this.moduleMetadataGenerator = Objects.requireNonNull(moduleMetadataGenerator);
+        this.processorSupport = Objects.requireNonNull(processorSupport);
+    }
+
+    // Serialize all
+
+    public void serializeAll(Path directory)
+    {
+        serializeAll(directory, true);
+    }
+
+    public void serializeAll(Path directory, boolean includeRootModule)
+    {
+        serializeAll(newSerializer(directory), includeRootModule);
+    }
+
+    public void serializeAll(ZipOutputStream stream)
+    {
+        serializeAll(stream, true);
+    }
+
+    public void serializeAll(ZipOutputStream stream, boolean includeRootModule)
+    {
+        serializeAll(newSerializer(stream), includeRootModule);
+    }
+
+    // Serialize modules
+
+    public void serializeModule(Path directory, String moduleName)
+    {
+        serializeModule(newSerializer(directory), moduleName);
+    }
+
+    public void serializeModule(ZipOutputStream stream, String moduleName)
+    {
+        serializeModule(newSerializer(stream), moduleName);
+    }
+
+    public void serializeModules(Path directory, String... moduleNames)
+    {
+        serializeModules(newSerializer(directory), moduleNames);
+    }
+
+    public void serializeModules(ZipOutputStream stream, String... moduleNames)
+    {
+        serializeModules(newSerializer(stream), moduleNames);
+    }
+
+    public void serializeModules(Path directory, Iterable<? extends String> moduleNames)
+    {
+        serializeModules(newSerializer(directory), moduleNames);
+    }
+
+    public void serializeModules(ZipOutputStream stream, Iterable<? extends String> moduleNames)
+    {
+        serializeModules(newSerializer(stream), moduleNames);
+    }
+
+    public void serializeModules(Path directory, Predicate<? super String> moduleFilter)
+    {
+        serializeModules(newSerializer(directory), moduleFilter);
+    }
+
+    public void serializeModules(ZipOutputStream stream, Predicate<? super String> moduleFilter)
+    {
+        serializeModules(newSerializer(stream), moduleFilter);
+    }
+
+    // Serialize elements
+
+    public void serializeElement(Path directory, String elementPath)
+    {
+        CoreInstance element = this.processorSupport.package_getByUserPath(Objects.requireNonNull(elementPath, "Invalid element path: null"));
+        if (element == null)
+        {
+            throw new IllegalArgumentException("Unknown element: " + elementPath);
+        }
+        this.fileSerializer.serializeElement(directory, element);
+        String moduleName = ModuleHelper.getElementModule(element);
+        if (moduleName != null)
+        {
+            updateModuleMetadata(directory, moduleName, Lists.immutable.with(elementPath), Sets.immutable.with(element.getSourceInformation().getSourceId()));
+        }
+    }
+
+
+    public void serializeElements(Path directory, String... elementPaths)
+    {
+        switch (elementPaths.length)
+        {
+            case 0:
+            {
+                return;
+            }
+            case 1:
+            {
+                serializeElement(directory, elementPaths[0]);
+                return;
+            }
+            default:
+            {
+                serializeElements(directory, Sets.mutable.with(elementPaths));
+            }
+        }
+    }
+
+    public void serializeElements(Path directory, Iterable<? extends String> elementPaths)
+    {
+        SetIterable<? extends String> elementPathSet = toElementSet(elementPaths);
+        if (elementPathSet.contains(null))
+        {
+            throw new NullPointerException("Invalid element path: null");
+        }
+        switch (elementPathSet.size())
+        {
+            case 0:
+            {
+                return;
+            }
+            case 1:
+            {
+                serializeElement(directory, elementPathSet.getAny());
+                return;
+            }
+            default:
+            {
+                MutableMap<String, Pair<MutableList<String>, MutableSet<String>>> elementsAndSourcesByModule = Maps.mutable.empty();
+                elementPathSet.forEach(elementPath ->
+                {
+                    CoreInstance element = this.processorSupport.package_getByUserPath(elementPath);
+                    if (element == null)
+                    {
+                        throw new IllegalArgumentException("Unknown element: " + elementPath);
+                    }
+                    String moduleName = ModuleHelper.getElementModule(element);
+                    if (moduleName != null)
+                    {
+                        Pair<MutableList<String>, MutableSet<String>> pair = elementsAndSourcesByModule.getIfAbsentPut(moduleName, () -> Tuples.pair(Lists.mutable.empty(), Sets.mutable.empty()));
+                        pair.getOne().add(elementPath);
+                        pair.getTwo().add(element.getSourceInformation().getSourceId());
+                        this.fileSerializer.serializeElement(directory, element);
+                    }
+                });
+                elementsAndSourcesByModule.forEachKeyValue((moduleName, moduleElementsAndSources) ->
+                        updateModuleMetadata(directory, moduleName, moduleElementsAndSources.getOne(), moduleElementsAndSources.getTwo()));
+            }
+        }
+    }
+
+    public void serializeElements(Path directory, Predicate<? super CoreInstance> elementFilter)
+    {
+        Objects.requireNonNull(elementFilter);
+        MutableMap<String, Pair<MutableList<String>, MutableSet<String>>> elementsAndSourcesByModule = Maps.mutable.empty();
+        GraphTools.getTopLevelAndPackagedElements(this.processorSupport).forEach(element ->
+        {
+            String moduleName = ModuleHelper.getElementModule(element);
+            if ((moduleName != null) && elementFilter.test(element))
+            {
+                Pair<MutableList<String>, MutableSet<String>> pair = elementsAndSourcesByModule.getIfAbsentPut(moduleName, () -> Tuples.pair(Lists.mutable.empty(), Sets.mutable.empty()));
+                pair.getOne().add(PackageableElement.getUserPathForPackageableElement(element));
+                pair.getTwo().add(element.getSourceInformation().getSourceId());
+                this.fileSerializer.serializeElement(directory, element);
+            }
+        });
+        elementsAndSourcesByModule.forEachKeyValue((moduleName, moduleElementsAndSources) ->
+                updateModuleMetadata(directory, moduleName, moduleElementsAndSources.getOne(), moduleElementsAndSources.getTwo()));
+    }
+
+    // Internals
+
+    private Serializer newSerializer(Path directory)
+    {
+        return new DirectorySerializer(this.fileSerializer, directory);
+    }
+
+    private Serializer newSerializer(ZipOutputStream stream)
+    {
+        return new ZipStreamSerializer(this.fileSerializer, stream);
+    }
+
+    private void serializeAll(Serializer serializer, boolean includeRootModule)
+    {
+        serializeModules(serializer, includeRootModule ? m -> true : ModuleHelper::isNonRootModule);
+    }
+
+    private void serializeModule(Serializer serializer, String moduleName)
+    {
+        GraphTools.getTopLevelAndPackagedElements(this.processorSupport)
+                .select(e -> ModuleHelper.isElementInModule(e, moduleName))
+                .forEach(serializer::serializeElement);
+        serializer.serializeModuleMetadata(this.moduleMetadataGenerator.generateModuleMetadata(ModuleHelper.resolveModuleName(moduleName)));
+    }
+
+    private void serializeModules(Serializer serializer, String... moduleNames)
+    {
+        switch (moduleNames.length)
+        {
+            case 0:
+            {
+                return;
+            }
+            case 1:
+            {
+                serializeModule(serializer, moduleNames[0]);
+                return;
+            }
+            default:
+            {
+                MutableSet<String> moduleNameSet = Sets.mutable.with(moduleNames);
+                if (moduleNameSet.remove(null))
+                {
+                    moduleNameSet.add(ModuleHelper.ROOT_MODULE_NAME);
+                }
+                serializeModules(serializer, moduleNameSet);
+            }
+        }
+    }
+
+    private void serializeModules(Serializer serializer, Iterable<? extends String> moduleNames)
+    {
+        serializeModules(serializer, toModuleNameSet(moduleNames));
+    }
+
+    private void serializeModules(Serializer serializer, SetIterable<? extends String> moduleNames)
+    {
+        // moduleNames should not contain null at this point
+        switch (moduleNames.size())
+        {
+            case 0:
+            {
+                return;
+            }
+            case 1:
+            {
+                serializeModule(serializer, moduleNames.getAny());
+                break;
+            }
+            default:
+            {
+                GraphTools.getTopLevelAndPackagedElements(this.processorSupport)
+                        .select(e -> moduleNames.contains(ModuleHelper.getElementModule(e)))
+                        .forEach(serializer::serializeElement);
+                this.moduleMetadataGenerator.generateModuleMetadata(moduleNames).forEach(serializer::serializeModuleMetadata);
+            }
+        }
+    }
+
+    private void serializeModules(Serializer serializer, Predicate<? super String> moduleFilter)
+    {
+        Objects.requireNonNull(moduleFilter);
+        MutableSet<String> moduleNames = Sets.mutable.empty();
+        GraphTools.getTopLevelAndPackagedElements(this.processorSupport).forEach(e ->
+        {
+            String moduleName = ModuleHelper.getElementModule(e);
+            if ((moduleName != null) && moduleFilter.test(moduleName))
+            {
+                moduleNames.add(moduleName);
+                serializer.serializeElement(e);
+            }
+        });
+        this.moduleMetadataGenerator.generateModuleMetadata(moduleNames).forEach(serializer::serializeModuleMetadata);
+    }
+
+    private void updateModuleMetadata(Path directory, String moduleName, ListIterable<String> elementsToUpdate, SetIterable<String> sourcesToUpdate)
+    {
+        ModuleMetadata initial = this.fileSerializer.moduleMetadataExists(directory, moduleName) ?
+                                 this.fileSerializer.deserializeModuleMetadata(directory, moduleName) :
+                                 ModuleMetadata.builder(moduleName).build();
+        ModuleMetadata updated = this.moduleMetadataGenerator.update(initial, elementsToUpdate, sourcesToUpdate);
+        this.fileSerializer.serializeModuleMetadata(directory, updated);
+    }
+
+    private static SetIterable<? extends String> toElementSet(Iterable<? extends String> elementPaths)
+    {
+        if (elementPaths instanceof SetIterable)
+        {
+            return (SetIterable<? extends String>) elementPaths;
+        }
+        if (elementPaths instanceof Set)
+        {
+            return SetAdapter.adapt((Set<? extends String>) elementPaths);
+        }
+        return Sets.mutable.withAll(elementPaths);
+    }
+
+    private static SetIterable<? extends String> toModuleNameSet(Iterable<? extends String> moduleNames)
+    {
+        if (moduleNames instanceof SetIterable)
+        {
+            SetIterable<? extends String> moduleNameSet = (SetIterable<? extends String>) moduleNames;
+            return moduleNameSet.contains(null) ?
+                   Sets.mutable.<String>withAll(moduleNameSet).without(null).with(ModuleHelper.ROOT_MODULE_NAME) :
+                   moduleNameSet;
+        }
+        if (moduleNames instanceof Set)
+        {
+            Set<? extends String> moduleNameSet = (Set<? extends String>) moduleNames;
+            return moduleNameSet.contains(null) ?
+                   Sets.mutable.<String>withAll(moduleNameSet).without(null).with(ModuleHelper.ROOT_MODULE_NAME) :
+                   SetAdapter.adapt(moduleNameSet);
+        }
+        MutableSet<String> moduleNameSet = Sets.mutable.withAll(moduleNames);
+        if (moduleNameSet.remove(null))
+        {
+            moduleNameSet.add(ModuleHelper.ROOT_MODULE_NAME);
+        }
+        return moduleNameSet;
+    }
+
+    public static Builder builder()
+    {
+        return new Builder();
+    }
+
+    public static class Builder
+    {
+        private FileSerializer fileSerializer;
+        private ModuleMetadataGenerator moduleMetadataGenerator;
+        private ProcessorSupport processorSupport;
+
+        private Builder()
+        {
+        }
+
+        public Builder withProcessorSupport(ProcessorSupport processorSupport)
+        {
+            this.processorSupport = processorSupport;
+            return this;
+        }
+
+        public Builder withFileSerializer(FileSerializer fileSerializer)
+        {
+            this.fileSerializer = fileSerializer;
+            return this;
+        }
+
+        public Builder withModuleMetadataGenerator(ModuleMetadataGenerator moduleMetadataGenerator)
+        {
+            this.moduleMetadataGenerator = moduleMetadataGenerator;
+            return this;
+        }
+
+        public PureCompilerSerializer build()
+        {
+            return new PureCompilerSerializer(this.fileSerializer, this.moduleMetadataGenerator, this.processorSupport);
+        }
+    }
+
+    private interface Serializer
+    {
+        void serializeElement(CoreInstance element);
+
+        void serializeModuleMetadata(ModuleMetadata moduleMetadata);
+    }
+
+    private static class DirectorySerializer implements Serializer
+    {
+        private final FileSerializer fileSerializer;
+        private final Path directory;
+
+        private DirectorySerializer(FileSerializer fileSerializer, Path directory)
+        {
+            this.fileSerializer = fileSerializer;
+            this.directory = directory;
+        }
+
+        @Override
+        public void serializeElement(CoreInstance element)
+        {
+            this.fileSerializer.serializeElement(this.directory, element);
+        }
+
+        @Override
+        public void serializeModuleMetadata(ModuleMetadata moduleMetadata)
+        {
+            this.fileSerializer.serializeModuleMetadata(this.directory, moduleMetadata);
+        }
+    }
+
+    private static class ZipStreamSerializer implements Serializer
+    {
+        private final FileSerializer fileSerializer;
+        private final ZipOutputStream stream;
+
+        private ZipStreamSerializer(FileSerializer fileSerializer, ZipOutputStream stream)
+        {
+            this.fileSerializer = fileSerializer;
+            this.stream = stream;
+        }
+
+        @Override
+        public void serializeElement(CoreInstance element)
+        {
+            this.fileSerializer.serializeElement(this.stream, element);
+        }
+
+        @Override
+        public void serializeModuleMetadata(ModuleMetadata moduleMetadata)
+        {
+            this.fileSerializer.serializeModuleMetadata(this.stream, moduleMetadata);
+        }
+    }
+}
