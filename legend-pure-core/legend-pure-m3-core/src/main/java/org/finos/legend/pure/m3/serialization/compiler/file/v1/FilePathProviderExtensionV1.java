13e9c602fecc5cf1f232c96dd7d4226ab93ed0a9
kevin-m-knight-gs <kevin.knight@gs.com>
2025-04-16 17:43:48 -0400

    Add file path provider

--- legend-pure-core/legend-pure-m3-core/src/main/java/org/finos/legend/pure/m3/serialization/compiler/file/v1/FilePathProviderExtensionV1.java
+++ legend-pure-core/legend-pure-m3-core/src/main/java/org/finos/legend/pure/m3/serialization/compiler/file/v1/FilePathProviderExtensionV1.java
@@ -19,14 +19,10 @@ import org.eclipse.collections.api.list.ImmutableList;
 import org.finos.legend.pure.m3.navigation.M3Paths;
 import org.finos.legend.pure.m3.navigation.PackageableElement.PackageableElement;
 import org.finos.legend.pure.m3.serialization.compiler.file.FilePathProviderExtension;
+import org.finos.legend.pure.m3.tools.FilePathTools;
 
 public class FilePathProviderExtensionV1 implements FilePathProviderExtension
 {
-    // UTF-16 uses 2 bytes per Java char, possibly plus a byte order marker of 2 bytes
-    // Since the name must be <= 255 bytes in UTF-16, 126 is the length limit for names (126 * 2 + 2 = 254)
-    private static final int NAME_LEN_LIMIT = 126;
-    private static final int SUFFIX_LEN = 16;
-
     private static final String ELEMENT_FILE_EXTENSION = ".pelt";
 
     private static final ImmutableList<String> MODULE_FILE_DIR = Lists.immutable.with("org", "finos", "legend", "pure", "module");
@@ -41,22 +37,9 @@ public class FilePathProviderExtensionV1 implements FilePathProviderExtension
     @Override
     public String getElementFilePath(String elementPath, String fsSeparator)
     {
-        if (PackageableElement.DEFAULT_PATH_SEPARATOR.equals(elementPath))
-        {
-            return M3Paths.Root + ELEMENT_FILE_EXTENSION;
-        }
-
-        StringBuilder builder = new StringBuilder(elementPath.length() + ELEMENT_FILE_EXTENSION.length());
-        int start = 0;
-        int sepLen = PackageableElement.DEFAULT_PATH_SEPARATOR.length();
-        int end;
-        while ((end = elementPath.indexOf(PackageableElement.DEFAULT_PATH_SEPARATOR, start)) != -1)
-        {
-            appendName(builder, elementPath, start, end, null).append(fsSeparator);
-            start = end + sepLen;
-        }
-
-        return appendName(builder, elementPath, start, elementPath.length(), ELEMENT_FILE_EXTENSION).toString();
+        return PackageableElement.DEFAULT_PATH_SEPARATOR.equals(elementPath) ?
+               (M3Paths.Root + ELEMENT_FILE_EXTENSION) :
+               FilePathTools.toFilePath(elementPath, PackageableElement.DEFAULT_PATH_SEPARATOR, fsSeparator, ELEMENT_FILE_EXTENSION);
     }
 
     @Override
@@ -64,45 +47,6 @@ public class FilePathProviderExtensionV1 implements FilePathProviderExtension
     {
         StringBuilder builder = new StringBuilder(moduleName.length() + MODULE_FILE_EXTENSION.length() + (MODULE_FILE_DIR.size() * fsSeparator.length()) + 24);
         MODULE_FILE_DIR.forEach(name -> builder.append(name).append(fsSeparator));
-        return appendName(builder, moduleName, 0, moduleName.length(), MODULE_FILE_EXTENSION).toString();
-    }
-
-    private static StringBuilder appendName(StringBuilder builder, String string, int start, int end, String extension)
-    {
-        int extLen = (extension == null) ? 0 : extension.length();
-        int len = (end - start) + extLen;
-        if (len < NAME_LEN_LIMIT)
-        {
-            // append the name, possibly plus extension, as it's below the limit
-            builder.append(string, start, end);
-            return (extension == null) ? builder : builder.append(extension);
-        }
-
-        // if the name is too long, append an initial segment plus a fixed length suffix computed from the overage
-        int overageStart = start + NAME_LEN_LIMIT - SUFFIX_LEN - extLen;
-        if (Character.isLowSurrogate(string.charAt(overageStart)) && Character.isHighSurrogate(string.charAt(overageStart - 1)))
-        {
-            // avoid splitting the string in the middle of a supplementary pair
-            overageStart--;
-        }
-        builder.ensureCapacity(builder.length() + (overageStart - start) + SUFFIX_LEN + extLen);
-        builder.append(string, start, overageStart);
-        String suffix = getOverageSuffix(string, overageStart, end);
-        for (int i = suffix.length(); i < SUFFIX_LEN; i++)
-        {
-            builder.append('0');
-        }
-        builder.append(suffix);
-        return (extension == null) ? builder : builder.append(extension);
-    }
-
-    private static String getOverageSuffix(String string, int start, int end)
-    {
-        long value = 0;
-        for (int i = start, cp; i < end; i += Character.charCount(cp))
-        {
-            value = (31 * value) + (cp = string.codePointAt(i));
-        }
-        return Long.toHexString(value);
+        return FilePathTools.appendFilePathName(builder, moduleName, MODULE_FILE_EXTENSION).toString();
     }
 }
