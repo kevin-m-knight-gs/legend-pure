2d7377da88ca4010b662b1396c54533203088236
kevin-m-knight-gs <kevin.knight@gs.com>
2025-06-02 15:50:56 -0400

    Add element loader

---
+++ legend-pure-core/legend-pure-m3-core/src/main/java/org/finos/legend/pure/m3/serialization/compiler/element/ElementBuilder.java
@@ -0,0 +1,36 @@
+// Copyright 2025 Goldman Sachs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.finos.legend.pure.m3.serialization.compiler.element;
+
+import org.finos.legend.pure.m3.coreinstance.Package;
+import org.finos.legend.pure.m3.coreinstance.meta.pure.metamodel.PackageableElement;
+import org.finos.legend.pure.m3.serialization.compiler.metadata.ConcreteElementMetadata;
+import org.finos.legend.pure.m3.serialization.compiler.metadata.MetadataIndex;
+import org.finos.legend.pure.m3.serialization.compiler.metadata.VirtualPackageMetadata;
+import org.finos.legend.pure.m3.serialization.compiler.reference.ReferenceIdResolver;
+import org.finos.legend.pure.m3.serialization.compiler.reference.ReferenceIdResolvers;
+import org.finos.legend.pure.m4.coreinstance.CoreInstance;
+
+import java.util.function.IntFunction;
+import java.util.function.Supplier;
+
+public interface ElementBuilder
+{
+    Package buildVirtualPackage(VirtualPackageMetadata metadata, MetadataIndex index, ReferenceIdResolvers referenceIds);
+
+    PackageableElement buildConcreteElement(ConcreteElementMetadata metadata, MetadataIndex index, ReferenceIdResolvers referenceIds, Supplier<? extends DeserializedConcreteElement> deserializer);
+
+    CoreInstance buildComponentInstance(InstanceData instanceData, MetadataIndex index, ReferenceIdResolver referenceIdResolver, IntFunction<? extends CoreInstance> internalIdResolver);
+}
