09a15cf017545352f975c9f19e9521e32643a300
kevin-m-knight-gs <kevin.knight@gs.com>
2025-09-30 10:46:20 -0400

    Add compiled mode lazy instances

--- legend-pure-runtime/legend-pure-runtime-java-engine-compiled/src/test/java/org/finos/legend/pure/runtime/java/compiled/generation/processors/support/coreinstance/TestCompiledElementBuilder.java
+++ legend-pure-runtime/legend-pure-runtime-java-engine-compiled/src/test/java/org/finos/legend/pure/runtime/java/compiled/generation/processors/support/coreinstance/TestCompiledElementBuilder.java
@@ -14,12 +14,74 @@
 
 package org.finos.legend.pure.runtime.java.compiled.generation.processors.support.coreinstance;
 
+import org.eclipse.collections.api.map.MapIterable;
+import org.finos.legend.pure.m3.navigation.M3Paths;
+import org.finos.legend.pure.m3.navigation.M3Properties;
 import org.finos.legend.pure.m3.serialization.compiler.element.AbstractPackageableElementBuilderTest;
 import org.finos.legend.pure.m3.serialization.compiler.element.ElementBuilder;
+import org.finos.legend.pure.m4.coreinstance.CoreInstance;
+import org.finos.legend.pure.runtime.java.compiled.execution.CompiledProcessorSupport;
 import org.finos.legend.pure.runtime.java.compiled.generation.JavaPackageAndImportBuilder;
+import org.finos.legend.pure.runtime.java.compiled.metadata.Metadata;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class TestCompiledElementBuilder extends AbstractPackageableElementBuilderTest
 {
+    @Test
+    @Override
+    public void testPrimitiveValueClassifiers()
+    {
+        CompiledProcessorSupport compiledProcessorSupport = new CompiledProcessorSupport(
+                Thread.currentThread().getContextClassLoader(),
+                new Metadata()
+                {
+                    @Override
+                    public void startTransaction()
+                    {
+                        throw new UnsupportedOperationException();
+                    }
+
+                    @Override
+                    public void commitTransaction()
+                    {
+                        throw new UnsupportedOperationException();
+                    }
+
+                    @Override
+                    public void rollbackTransaction()
+                    {
+                        throw new UnsupportedOperationException();
+                    }
+
+                    @Override
+                    public CoreInstance getMetadata(String classifier, String id)
+                    {
+                        if (M3Paths.PrimitiveType.equals(classifier))
+                        {
+                            return elementLoader.loadElement(id);
+                        }
+                        throw new UnsupportedOperationException();
+                    }
+
+                    @Override
+                    public MapIterable<String, CoreInstance> getMetadata(String classifier)
+                    {
+                        throw new UnsupportedOperationException();
+                    }
+
+                    @Override
+                    public CoreInstance getEnum(String enumerationName, String enumName)
+                    {
+                        throw new UnsupportedOperationException();
+                    }
+                },
+                null);
+        CoreInstance stringType = this.elementLoader.loadElement(M3Paths.String);
+        CoreInstance stringTypeName = stringType.getValueForMetaPropertyToOne(M3Properties.name);
+        Assert.assertSame(stringType, compiledProcessorSupport.getClassifier(stringTypeName));
+    }
+
     @Override
     protected ElementBuilder newElementBuilder()
     {
